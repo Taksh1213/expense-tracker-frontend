c8c0435a33e191631871801154bba1072d1f077c
Taksh1213 <jiyanshutaksh@gmail.com>
2025-11-13 20:54:59 +0530

    Frontend build fixed and ready for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,26 @@
 import path from "path";
 import { fileURLToPath } from "url";
 
-// Fix __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   turbopack: {
-    root: path.resolve(__dirname),
+    root: __dirname,
   },
-  distDir: ".next",
+
   images: {
-    domains: ["expense-tracker-backend-vsxb.onrender.com", "localhost"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "expense-tracker-backend-vsxb.onrender.com",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
   },
 };
 
