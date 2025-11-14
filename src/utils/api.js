01a095953fb011785eb35eb134490819b1fee9ad
Taksh1213 <jiyanshutaksh@gmail.com>
2025-11-14 18:13:32 +0530

    Fix API URlr

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://expense-tracker-backend-vsxb.onrender.com",
+  baseURL: "https://expense-tracker-backend-vsxb.onrender.com/api",
   withCredentials: true,
 });
 
